9a75811e9408a0f07168a82b987744de626057f4
Malarvizhiparvathi <36284938+Malarvizhiparvathi@users.noreply.github.com>
2018-05-21 14:11:44 +0530

    Update array smallest.py

--- array smallest.py
+++ array smallest.py
@@ -1,7 +1,4 @@
-def kSmallest(arr, k):
-    arr.sort(reverse=False)
-    for i in range(k):
-        print (arr[i],end=" ") 
-arr = [1, 23, 12, 9, 30, 2, 50]
-k = int(input(""))
-kSmallest(arr, k)
+a=str(input("")
+num=str(input(""))
+b=map(str,num.split(" "))
+print (min(b))
